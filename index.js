4a30e2510db082d5462173a71bdb5e80fe9d00e7
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-09-09 22:37:30 +0400

    Revert "remove safety settings"
    This reverts commit 2b6f0ad5886bc3b940b68b14387001bbb22abccf.

--- index.js
+++ index.js
@@ -203,24 +203,6 @@ Your primary goal is to be a silent, accurate assistant. If in doubt, do not res
 		contents: [{
 			parts: [{ text: prompt }]
 		}],
-		safetySettings: [
-			{
-				category: 'HARM_CATEGORY_HARASSMENT',
-				threshold: 'BLOCK_NONE'
-			},
-			{
-				category: 'HARM_CATEGORY_HATE_SPEECH',
-				threshold: 'BLOCK_NONE'
-			},
-			{
-				category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
-				threshold: 'BLOCK_NONE'
-			},
-			{
-				category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
-				threshold: 'BLOCK_NONE'
-			}
-		]
 	};
 
 	
